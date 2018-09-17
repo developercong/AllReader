515cf2d698a3a6a9ad8574c8e12731aad67255f0
developercong <qaz889025>
2018-09-17 21:36:30 +0800

    [0.1.0] [WN] add array stack

---
+++ app/src/main/java/com/congzibank/allreader/utils/GeneralArrayStack.java
@@ -0,0 +1,44 @@
+package com.congzibank.allreader.utils;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * 用数组实现栈
+ * @author wangcong
+ * push peek pop
+ */
+public class GeneralArrayStack<T> {
+
+    public static final int DEFAULT_SIZE = 12;
+    private T[] arrays;
+    private int count;
+
+    public GeneralArrayStack(Class<T> type) {
+        this(type, DEFAULT_SIZE);
+    }
+
+    public GeneralArrayStack(Class<T> type, int capacity) {
+        arrays = (T[]) Array.newInstance(type, capacity);
+        count = 0;
+    }
+
+    public void push(T value){
+        arrays[count++] = value;
+    }
+
+    public T peek(){
+        if (count > 0){
+            return arrays[count - 1];
+        }else {
+            return null;
+        }
+    }
+
+    public T pop() {
+        if (count == 0) return null;
+        T value = arrays[count - 1];
+        count--;
+        return value;
+    }
+}
